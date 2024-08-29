112f7384325401aeea777cfee0e7c0a99a786e59
Harshal Gohel <hg@simonwunderlich.de>
2024-08-29 14:32:15 +0000

    add support for twentyeight port switch and rtl931x Fix i2c (mux) and external gpio for rtl931x Fix vlan set tagged for rtl931x
    Signed-off-by: Harshal Gohel <hg@simonwunderlich.de>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -16,3 +16,16 @@ define Device/zyxel_xgs1250-12
 	uImage gzip
 endef
 TARGET_DEVICES += zyxel_xgs1250-12
+define Device/eight_port
+  SOC := rtl9302
+  UIMAGE_MAGIC := 0x93000000
+  DEVICE_VENDOR := None
+  DEVICE_MODEL := eight_port
+  IMAGE_SIZE := 31744k
+  KERNEL_INITRAMFS := \
+	kernel-bin | \
+	append-dtb | \
+	gzip | \
+	uImage gzip
+endef
+TARGET_DEVICES += eight_port
--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -1,1 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
+define Device/twentyeight_port
+  SOC := rtl9312
+  UIMAGE_MAGIC := 0x93100000
+  DEVICE_VENDOR := None
+  DEVICE_MODEL := twentyeight_port
+  IMAGE_SIZE := 13376k
+  KERNEL_INITRAMFS := \
+	kernel-bin | \
+	append-dtb | \
+	lzma | \
+	uImage lzma
+endef
+TARGET_DEVICES += twentyeight_port
